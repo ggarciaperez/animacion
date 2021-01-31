c21c2834a8b48012526091ddef1aabd54e7ef8d9
Guillermo <ggarciaperez@danielcastelao.org>
2021-01-31 22:10:31 +0100

    added follow camera

---
+++ CamaraPegada.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraPegada : MonoBehaviour
+{
+
+    public GameObject macaco;
+    private Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - macaco.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position= macaco.transform.position + offset;    
+    }
+}
